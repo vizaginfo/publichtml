6cd40b7035ee69f4089942b9377e60da7148873d
yaswanth89kumar <yaswant207@gmail.com>
2016-03-18 14:29:47 +0530

    login and location pages
    Files of login page and location pages

---
+++ vizag/assets/js/modules.js
@@ -0,0 +1,31 @@
+var app = angular.module('vizag',['ngRoute']); 
+app.config(function($routeProvider){  
+	$routeProvider
+	.when('/',
+		{   
+			templateUrl:'users/login.html',
+			controller:'UserController'
+		}
+	)
+	.when('/location',
+		{   
+			templateUrl:'users/location.html',
+			controller:'LocationController'
+		}
+	)
+	.otherwise({
+			redirectTo : "/"
+	})	
+});
+app.controller("UserController",function($scope,$route){
+	$scope.data={
+					//customProperty:$route.current.companyName,
+					//queryString: $route.current.params.city, 
+					//pathParams : $route.current.pathParams.id
+				};
+
+	$scope.loadedTime=new Date();
+	
+	
+
+});
