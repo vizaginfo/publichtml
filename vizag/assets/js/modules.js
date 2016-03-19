e44ee77d199aab06d9749d60fe2d58adc18cd3e1
yaswanth89kumar <yaswant207@gmail.com>
2016-03-19 10:58:21 +0530

    login request

--- vizag/assets/js/modules.js
+++ vizag/assets/js/modules.js
@@ -10,6 +10,27 @@ app.config(function ($routeProvider) {
         redirectTo: "/"
     })
 });
-app.controller("UserController", function ($scope, $route) {
+app.controller("UserController", function ($scope, $route, $http) {
     $scope.loadedTime = new Date();
+    $scope.formData = {};
+    $scope.processForm = function () {
+        $http({
+                method: 'POST',
+                url: 'process.php',
+                data: $.param($scope.formData), // pass in data as strings
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                } // set the headers so angular passing info as form data (not request payload)
+        })
+        .success(function (data) {
+            if (!data.success) {
+                // if not successful, bind errors to error variables
+                $scope.errormyName = data.errors.myName;
+                $scope.errorPassword = data.errors.password;
+            } else {
+                // if successful, bind success message to message
+                $scope.message = data.message;
+            }
+        });
+    };
 });
